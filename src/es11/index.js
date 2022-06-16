744ea86bb0ee01ba0cd5b7ec0de37ca76c1b1c26
Libardo Arroyave <laastos@gmail.com>
2022-06-16 14:03:14 -0500

    WIP

---
+++ src/es11/index.js
@@ -0,0 +1,78 @@
+/**
+ * Dynamic import para importar archivos 
+ * Suponiendo que se quiere importar un HTML
+ */
+const button = document.getElementById("btn");
+//Aqui se crea el boton
+button.addEventListener("click", async function(){
+    //se hace la importacion del file.js
+    const module = await import("./file.js");
+    module.hello();
+});
+
+/**
+ * Por aparte se crea el file.js donde se crea un export que es lo
+ * que va a suceder cuando se de click en el boton.
+ */
+export function hello(){
+    console.log("Hola Mundo!")
+}
+
+/**
+ * 🔢 Big INT: permite trabajar con numeros mayores a 2^53
+ */
+{
+  // 1- Añadiendo una "n" al final activas el big int en el valor
+  const aBigNumber = 9007199254740991n;
+
+  // 2- Con el metodo BigInt activas el valor
+  const anotherBigNumber = BigInt(9007199254740991);
+
+  console.log(aBigNumber);
+  console.log(anotherBigNumber);
+}
+
+/**
+ * 🤝 Promise All Settled
+ */
+{
+  const promise1 = new Promise((resolve,reject) => reject("reject"));
+  const promise2 = new Promise((resolve,reject) => resolve("resolve"));
+  const promise3 = new Promise((resolve,reject) => resolve("resolve 1"));
+
+
+  Promise.allSettled([promise1, promise2, promise3])
+      .then(response => console.log(response));
+}
+
+/**
+ * 🌎 Global This
+ */
+
+console.log(window);
+console.log(globalThis);
+
+/**
+ * 🔍 Nuevo operador lógico: null operator
+ */
+{
+  const fooo = null ?? 'default string';
+  console.log(fooo);
+
+  const fooo2 = 'not null' ?? 'default string';
+  console.log(fooo2)
+}
+
+/**
+ * ⛓ Optional chaining
+*/
+{
+  const user = {};
+  console.log(user?.profile?.email);
+
+  if(user?.profile?.email) {
+      console.log('email')
+  } else{
+      console.log('fail')
+  }
+}
